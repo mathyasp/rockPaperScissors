fcc31b82cc6c9c98072919e165b59b589324a2c6
mathyasp <119142489+mathyasp@users.noreply.github.com>
2023-01-19 16:50:14 -0500

    Make function to play rock paper scissors

--- myScripts.js
+++ myScripts.js
@@ -9,4 +9,33 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+// Write a function that plays a single round of Rock Paper Scissors
+// The function should return a string that declares the winner
+function playRound(playerSelection, computerSelection) {
+    let p = capitalize(playerSelection);
+    let c = computerSelection;
+    if (p === c) {
+        return "Tie! Play again.";
+    } else {
+        if ((p === "Rock" && c === "Scissors") || (p === "Paper" && c === "Rock") || (p === "Scissors" && c === "Paper")) {
+            return `You Win! ${p} beats ${c}`;
+        } else {
+            return `You Lose! ${c} beats ${p}`;
+        }
+    }
+}
+
+// Make the playerSelection case-insensitive
+function capitalize(word) {
+    let firstLetter = word.charAt(0);
+    let restOfString = word.slice(1);
+    return (firstLetter.toUpperCase() + restOfString.toLowerCase());
+}
+
+// The function should take two parameters playerSelection and computerSelection
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
 console.log(getComputerChoice());
+console.log(playRound(playerSelection, computerSelection));
